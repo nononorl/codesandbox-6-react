989f77649ff0b0a5d1b8e855fd6a2f4468525f29
nononorl <86286789+nononorl@users.noreply.github.com>
2021-07-26 01:36:18 +0900

    コミット

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("さいしょ");
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
 
   const onClickCountUp = () => {
     setNum(num + 1);
   };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  if (num > 0) {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }
 
   return (
     <>
@@ -16,7 +28,7 @@ const App = () => {
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
       <br />
-      <button>on/off</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
       {faceShowFlag && <p>( ｀ー´)ノ</p>}
     </>
